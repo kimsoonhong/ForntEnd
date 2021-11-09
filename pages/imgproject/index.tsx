e0f87a0d6e9950023683995b3d03ad97501b4cbc
김순홍 <shong1332@naver.com>
2021-11-09 16:21:11 +0900

    2.4.4

--- pages/imgproject/index.tsx
+++ pages/imgproject/index.tsx
@@ -1,6 +1,7 @@
 import Project from "../../src/components/unit/imgProject/src/components/unit/project/project.container";
 import { GlobalContext } from "../../pages/_app";
 import { useContext } from "react";
+
 export default function ImgProject() {
   const { setIsLayout } = useContext(GlobalContext);
   setIsLayout(false);
