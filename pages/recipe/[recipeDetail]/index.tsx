c82490ade0fd7cdb705abdd15b350b429d85c64a
김순홍 <shong1332@naver.com>
2021-11-11 15:55:56 +0900

    ssr

--- pages/recipe/[recipeDetail]/index.tsx
+++ pages/recipe/[recipeDetail]/index.tsx
@@ -1,6 +1,6 @@
 import Recipedetail from "../../../src/components/unit/recipe/src/components/unit/recipeDetail/recipedetail.container";
-// import Head from "next/head";
-// import axios from "axios";
+import Head from "next/head";
+import axios from "axios";
 import { useContext } from "react";
 import { GlobalContext } from "../../../pages/_app";
 
@@ -16,27 +16,27 @@ export default function recipeDetailPage(props: Iprops) {
   setIsLayout(false);
   return (
     <>
-      {/* <Head>
+      <Head>
         <meta property="og:title" content={props.recipelist.name} />
         <meta property="og:contents" content={props.recipelist.description} />
         <meta property="og:images" content={`${props.recipelist.picture}`} />
-      </Head> */}
+      </Head>
       <Recipedetail />
     </>
   );
 }
 
-// export const getServerSideProps = async (context: any) => {
-//   let recipelist = [];
-//   try {
-//     const recipeID = context.query.recipeDetail;
-//     const getRecipeList: any = await axios.get(
-//       `https://asia-northeast1-sharexpere-prod.cloudfunctions.net/recipe/${recipeID}`
-//     );
-//     recipelist = getRecipeList.data;
-//   } catch (error) {
-//     console.log(error.message);
-//   }
+export const getServerSideProps = async (context: any) => {
+  let recipelist = [];
+  try {
+    const recipeID = context.query.recipeDetail;
+    const getRecipeList: any = await axios.get(
+      `https://asia-northeast1-sharexpere-prod.cloudfunctions.net/recipe/${recipeID}`
+    );
+    recipelist = getRecipeList.data;
+  } catch (error) {
+    console.log(error.message);
+  }
 
-//   return { props: { recipelist } };
-// };
+  return { props: { recipelist } };
+};
